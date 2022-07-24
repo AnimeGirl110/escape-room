60f06c4d283ca71577a5c4fdc56a30e7c82eb4ae
AnimeGirl110 <djoctowoomy@gmail.com>
2022-07-24 16:00:33 -0400

    added hands/camera

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    private const string HORIZONTAL_INPUT = "Horizontal";
-    public const float ROTATE_SPEED = 50f;
-    
+    private const float TRANSLATE_VEL = 10f;
+    private const string MOUSE_X = "Mouse X";
+    private const string MOUSE_Y = "Mouse Y";
+    private const string VERTICAL = "Vertical";
+    private const string HORIZONTAL = "Horizontal";
+
+    private Vector2 rotation = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Input.GetAxis(HORIZONTAL_INPUT) * ROTATE_SPEED * Time.deltaTime);
+        transform.Translate(
+            (
+                Vector3.forward * Input.GetAxis(VERTICAL) + 
+                Vector3.right * Input.GetAxis(HORIZONTAL) +
+                Vector3.up * (
+                    (Input.GetKey(KeyCode.E) ? 1 : 0) +
+                    (Input.GetKey(KeyCode.Q) ? -1 : 0)
+                )
+            ) * TRANSLATE_VEL * Time.deltaTime
+        );
+        if (!Input.GetMouseButton(0)) {
+            return;
+        }
+        rotation.x += Input.GetAxis(MOUSE_X);
+        rotation.y += Input.GetAxis(MOUSE_Y);
+        Quaternion xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        Quaternion yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+        transform.localRotation = xQuat * yQuat;
     }
 }
