cd590e9f4c07e18d17b00987017d5748017e3b71
AnimeGirl110 <djoctowoomy@gmail.com>
2022-07-29 20:13:23 -0400

    added doors-- check playerscript. updated puzzle

--- Assets/Leah's Room/Scripts/CameraBehavior.cs
+++ Assets/Leah's Room/Scripts/CameraBehavior.cs
@@ -11,7 +11,9 @@ namespace Leah {
     void Start()
     {
       target = GameObject.Find("PlayerObj").transform;
+      //does this work?
     }
+
     private void LateUpdate()
     {
       transform.position = target.position;
--- Assets/Leah's Room/Scripts/PuzzleBehavior.cs
+++ Assets/Leah's Room/Scripts/PuzzleBehavior.cs
@@ -7,10 +7,11 @@ namespace Leah {
     {
         public GameObject tilePrefab;
         private List<GameObject> tiles = new List<GameObject>();
-        public int numTilesSide = 6;
-        public float tileInterval = 1.05f;
-        public float tileOffsetX = 0;
-        public float tileOffsetY = 0;
+        public int numTilesSide = 5;
+        public float tileInterval = 1;
+        public float tileOffsetX = -4.5f;
+        public float tileOffsetY = -4.5f;
+        public bool isPlayingPuzzle = true;
 
         // Start is called before the first frame update
         void Start()
@@ -19,20 +20,29 @@ namespace Leah {
                 for (int row = 0; row < numTilesSide; row++) {
                   tiles.Add(Instantiate(
                     tilePrefab, 
-                    new Vector3(col * tileInterval + tileOffsetX, 0.3f, row * tileInterval + tileOffsetY), 
+                    new Vector3(col * tileInterval + tileOffsetX, 0.2f, row * tileInterval + tileOffsetY), 
                     Quaternion.identity)); 
                 }
-            }   
-            //instantiate a bunch of tiles
+            }
+            ResetAllTiles();
         }
 
         public void TileUpdate() {
-            bool allArePressed = true;
             for (int i = 0; i < tiles.Count; i++) {
-                    if (!tiles[i].GetComponent<TileBehavior>().GetIsPressed()) {
-                        allArePressed = false;
-                        //reset all tiles
-                    }
+                if (!tiles[i].GetComponent<TileBehavior>().GetIsPressed()) { //checks all tiles
+                    return;
+                }
+            } 
+            isPlayingPuzzle = false;
+            GameObject.Find("Door_Joint").GetComponent<Animator>().Play("DoorOpening", 0, 0);
+            //open door
+        }
+
+        public void ResetAllTiles() {
+            for (int i = 0; i < tiles.Count; i++) {
+                GameObject tile = tiles[i];
+                tile.GetComponent<TileBehavior>().SetIsPressed(false);
+                tile.GetComponent<Renderer>().material = tile.GetComponent<TileBehavior>().offMat;
             }
         }
     }
